4b153d7e9a60f87e6ef98ca1bff3ec9c275f5ecc
PabloMendozaxd <jose.mendozaji@my.unitec.edu.mx>
2022-03-21 18:13:59 -0600

    exists an object with roman value

---
+++ index.js
@@ -0,0 +1,13 @@
+
+const romanValues = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000,
+};
+const romanToInteger=()=>'hi';
+
+module.exports={romanToInteger,romanValues};
